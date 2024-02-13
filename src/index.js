44bff46d4f15705b7862a84e5320c4876c0d1291
Oleh <olehsjs@gmail.com>
2024-02-13 09:35:27 -0600

    Fixed wrong render settings in production

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import DocRender from './DocRender';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <DailyActivityReport />
+    <App />
   </React.StrictMode>
 );
 
